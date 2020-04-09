6affc915f78178cd3badf03738258cdb8e190288
Timo Tyrväinen <gr186703@gradia.fi>
2020-04-09 11:48:32 +0300

    tehtäviä

---
+++ project1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset=new Vector3(02,7,-9);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // kamera pelaajan taakse lisäämällä pelaajan sijainti
+        transform.position = player.transform.position + offset;
+        
+    }
+}
---
+++ project1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   private float speed= 10f;
+   private float turnSpeed=30f;
+   private float horizontalInput;
+   public float forwardInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput= Input.GetAxis("Horizontal");
+        forwardInput=Input.GetAxis("Vertical");
+       // liikutetaan autoa eteenpäin
+       transform.Translate(Vector3.forward*Time.deltaTime*speed * forwardInput);
+       transform.Rotate(Vector3.up,  Time.deltaTime * turnSpeed * horizontalInput);  
+    }
+}
