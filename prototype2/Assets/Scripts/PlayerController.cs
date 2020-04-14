1fbfbae90e898be569191b1c398a8245f30a05eb
Timo Tyrväinen <gr186703@gradia.fi>
2020-04-14 19:47:02 +0300

    uusia

---
+++ prototype2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 20.0f;
+    public GameObject Projectileprefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Pidetään pelaaja alueella
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+         if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+       horizontalInput= Input.GetAxis("Horizontal"); 
+       transform.Translate(Vector3.right*horizontalInput*Time.deltaTime*speed);
+       if (Input.GetKeyUp(KeyCode.Space))
+       {
+           //Paina välilyönti
+           Instantiate(Projectileprefab, transform.position, Projectileprefab.transform.rotation);
+       }
+    }
+}
