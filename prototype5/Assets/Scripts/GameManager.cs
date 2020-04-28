1562b4514ab90f92086fe6b1760d216ddbc77067
Timo Tyrväinen <gr186703@gradia.fi>
2020-04-28 10:56:49 +0300

    Klikkailu pelit

---
+++ prototype5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;// lista targets
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    public GameObject titleScreen;
+    public bool gameActive;
+    private int score;
+    private float spawnRate=1; //esineiden luomis väli sekunneissa
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+               
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTargets()
+    {// luodaan esineet pelikentälle
+        while(gameActive)
+        {
+        yield return new WaitForSeconds(spawnRate);
+        int index=Random.Range(0,targets.Count);
+        Instantiate(targets[index]);
+       
+        }
+        
+    }
+   public void UpdateScore(int scoreAdd)
+    {
+        score += scoreAdd;
+        scoreText.text="Score: " +score;
+    }
+    public void GameOver()
+    {
+        restartButton.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        gameActive=false;
+    }
+    public void RestartGame()
+    {        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);        
+    }
+    public void StartGame(int difficulty)
+    {
+        gameActive=true;
+        titleScreen.gameObject.SetActive(false);
+        spawnRate /=difficulty;
+        StartCoroutine(SpawnTargets());//kutsutaan luonti metodia
+        score=0;
+        UpdateScore(0);
+        
+    }
+}
---
+++ prototype5/Assets/Scripts/Targets.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Targets : MonoBehaviour
+{
+    private Rigidbody targetRb;
+    private GameManager gameManager;
+    private float minSpeed=12;
+    private float maxSpeed=16;
+    private float torque=5;
+    private float xRange=4;
+    private float ySpawnPos=-2;
+    public int pointValue;
+    public ParticleSystem explosion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb=GetComponent<Rigidbody>();
+        gameManager=GameObject.Find("Game Manager").GetComponent<GameManager>();
+        targetRb.AddForce(Vector3.up*Random.Range(minSpeed,maxSpeed),ForceMode.Impulse);
+       
+        targetRb.AddTorque(RandomTorque(),RandomTorque(),RandomTorque(),ForceMode.Impulse);
+
+        transform.position=RandomPos();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        if(gameManager.gameActive)
+        {
+            Destroy(gameObject );
+            Instantiate(explosion,transform.position,explosion.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
+      
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        if(!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+        
+    }
+    Vector3 RandomForce()
+    {
+        //kuinka nopeasti tavarat lentää
+       return Vector3.up*Random.Range(minSpeed,maxSpeed);
+    }
+    float RandomTorque()
+    {
+         //Määrätään kuinka tavarat pyörii akselinsa ympäri
+       return Random.Range(-torque,torque); 
+    }
+    Vector3 RandomPos()
+    {
+        //Mistä kohdasta tavarat ilmestyy (vasen,oikea),korkeus
+        return new Vector3(Random.Range(-xRange,xRange),ySpawnPos);
+    }
+}
